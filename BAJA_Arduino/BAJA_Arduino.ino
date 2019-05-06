4055181f7fa27f26154eb9d1820aa298f77e1608
josuemendez94 <40776830+josuemendez94@users.noreply.github.com>
2019-05-06 19:24:39 -0400

    Added comments

--- BAJA_Arduino/BAJA_Arduino.ino
+++ BAJA_Arduino/BAJA_Arduino.ino
@@ -1,3 +1,8 @@
+#include <RadioHead.h>
+
+#include <RH_RF95.h>
+
+
 /*
  * Contributed Libraries
  * Use "Manage Libraries" menu option to download these
@@ -29,18 +34,18 @@
 /* Arduino Micro */
 #if defined (__AVR_ATmega32U4__) 
   /* G0 Pin - Has to be an external interrupt pin (RFM95_INT)*/
-  #define RFM95_INT     7 
-  #define RFM95_CS      8 
-  #define RFM95_RST     4
+  #define RFM95_INT     0
+  #define RFM95_CS      4 
+  #define RFM95_RST     3
   #define LED           13
 #endif
 
 /* Arduino Uno | Arduino Nano */
 #if defined (__AVR_ATmega328P__) 
   /* G0 Pin - Has to be an external interrupt pin (RFM95_INT)*/
-  #define RFM95_INT     2 
+  #define RFM95_INT     3 
   #define RFM95_CS      4  
-  #define RFM95_RST     3  
+  #define RFM95_RST     2  
   #define LED           13
 #endif
 
@@ -53,19 +58,20 @@
  #define DEBUGGING_DAC
  #define DEBUGGING_RPM
 
- 
-
  #include <Wire.h>
 
+ /* User defined parameters for DAC */
  #define  DAC_I2C_ADDRESS         0x62
  #define  DAC_MIN                 0.00
  #define  DAC_MAX                 5.00
 
+ /* User defined parameters for RPM  */
  #define  RPM_INPUT_PIN           13
  #define  RPM_SAMPLING_AVERAGE    2
  #define  RPM_MIN                 10
  #define  RPM_MAX                 50
 
+ /* RFM95 Frquency 915.0, 434.0, or 868.0 MHz */
  #define RF95_FREQ                915.0
 
  RH_RF95 rf95(RFM95_CS, RFM95_INT);
@@ -110,7 +116,7 @@ void setup() {
    * of the RPM sensor input
    */
    #ifdef DEBUGGING_SETUP
-    Serial.print("Initializing FreqMeasure on pin ");
+    Serial.print(F("Initializing FreqMeasure on pin "));
     Serial.println(RPM_INPUT_PIN);
    #endif
    FreqMeasure.begin();
@@ -120,7 +126,7 @@ void setup() {
     * Configure Wire (I2C) for the DAC
     */
    #ifdef DEBUGGING_SETUP
-    Serial.println("Initializing Wire for the DAC");
+    Serial.println(F("Initializing Wire for the DAC"));
    #endif
    DAC.begin(DAC_I2C_ADDRESS);
 
@@ -129,7 +135,7 @@ void setup() {
 
    //Initialize the IMU
    if(!IMU.begin()){
-    Serial.println("Failed to connect to the IMU!");
+    Serial.println(F("Failed to connect to the IMU!"));
    }
 
    delay(1000);
@@ -139,7 +145,7 @@ void setup() {
     * Configure RFM9X LoRa radio 
     */
    #ifdef DEBUGGING_SETUP
-    Serial.println("Initializing LoRa TX Test!");
+    Serial.println(F("Initializing LoRa TX Test!"));
    #endif 
 
    pinMode(RFM95_RST, OUTPUT);
@@ -153,19 +159,19 @@ void setup() {
    delay(10);
 
    while (!rf95.init()) {
-    Serial.println("LoRa radio init failed");
+    Serial.println(F("LoRa radio init failed"));
     while (1);
   }
   
-   Serial.println("LoRa radio init OK!");
+   Serial.println(F("LoRa radio init OK!"));
 
   /* Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM */
   if (!rf95.setFrequency(RF95_FREQ)) {
-    Serial.println("setFrequency failed");
+    Serial.println(F("setFrequency failed"));
     while (1);
   }
 
-  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  Serial.print(F("Set Freq to: ")); Serial.println(RF95_FREQ);
 
   /* 
    * The default transmitter power is 13dBm, using PA_BOOST.
@@ -175,13 +181,14 @@ void setup() {
   rf95.setTxPower(23, false);
 
    #ifdef DEBUGGING_SETUP
-    Serial.println("Initialization done");
+    Serial.println(F("Initialization done"));
    #endif
 }
 
 float CurrentFrequency = 0;
 float DACVoltage = 0;
 
+/* Converts frequency to a DAC voltage value */
 float DAC_FrequencyToVoltage(float Frequency){
   float RPM_Range = RPM_MAX - RPM_MIN;
   float DAC_Range = DAC_MAX - DAC_MIN;
@@ -190,17 +197,20 @@ float DAC_FrequencyToVoltage(float Frequency){
   return Voltage;
 }
 
+/* Converts voltage value to DAC voltage output  */
 uint16_t DAC_VoltageToValue(float Voltage){
   float ratio = 4095/5.0;
   float Value = Voltage * ratio;
   return (uint16_t)(Value);
 }
 
+/* Update DAC voltage output */
 void UpdateDAC(){
   DACVoltage = DAC_FrequencyToVoltage(CurrentFrequency);
   DAC.setVoltage(DAC_VoltageToValue(DACVoltage), false);
 }
 
+/* Updats frequency of hall effect sensor */
 void SetFrequency(float Frequency){
   //Clamp the frequency at the max and min RPMs
   if(Frequency < RPM_MIN){
@@ -217,7 +227,7 @@ void SetFrequency(float Frequency){
   UpdateDAC();
 
   #ifdef DEBUGGING_RPM
-    Serial.print("Frequency set to ");
+    Serial.print(F("Frequency set to "));
     Serial.println(Frequency);
   #endif
 }
@@ -243,67 +253,80 @@ void GetFreqMeasurement(){
   }
 }
 
+int index = 0;
+char ByteArray[RH_RF95_MAX_MESSAGE_LEN] = {0};
+char Tab[] = " ";
+
+// Adds data to array
+void AddByteArray(char BA[], int size){
+  strcat(ByteArray, BA);
+  strcat(ByteArray, Tab);
+  index = size;
+}
+
+/* Reads DOF sensor and prints data out to serial monitor */
 void ReadDOFAndPrint(){
+  //delay(1000);
+  
   sensors_event_t event;
   IMU.getEvent(&event, Adafruit_BNO055::VECTOR_GYROSCOPE);
 
   char radioPacket[10];
-  char buff[255];
   String result = "";
-  
-  Serial.print("X: ");
+
+  memset(ByteArray, 0, sizeof(ByteArray));
+
+  Serial.print(F("\tX: "));
   Serial.print(event.orientation.x, 4);
-  Serial.print("\tY: ");
+  Serial.print(F("\tY: "));
   Serial.print(event.orientation.y, 4);
-  Serial.print("\tZ: ");
+  Serial.print(F("\tZ: "));
   Serial.print(event.orientation.z, 4);
 
   sensors_event_t AccEvent;
   IMU.getEvent(&AccEvent, Adafruit_BNO055::VECTOR_ACCELEROMETER);
-  Serial.print("\taX: ");
+  Serial.print(F("\taX: "));
   Serial.print(AccEvent.acceleration.x, 4);
-  Serial.print("\taY: ");
+  Serial.print(F("\taY: "));
   Serial.print(AccEvent.acceleration.y, 4);
-  Serial.print("\taZ: ");
+  Serial.print(F("\taZ: "));
   Serial.println(AccEvent.acceleration.z, 4);
+
+  // Converts float to string 
+  dtostrf(CurrentFrequency, 1, 1, radioPacket);
+  AddByteArray(radioPacket, sizeof(radioPacket));
   
-  /*dtostrf(CurrentFrequency, 1, 1, radioPacket);
-  result += radioPacket;
-  result += ",";
-  strcat(buff,radioPacket);
   dtostrf(event.orientation.x, 3, 4, radioPacket);
-  result += radioPacket;
-  result += ",";
-  Serial.println(radioPacket);
-  strcat(buff,radioPacket);
-  Serial.println(buff);
+  AddByteArray(radioPacket, sizeof(radioPacket));
+  
   dtostrf(event.orientation.y, 3, 4, radioPacket);
-  result += radioPacket;
-  result += ",";
+  AddByteArray(radioPacket, sizeof(radioPacket));
+  
   dtostrf(event.orientation.z, 3, 4, radioPacket);
-  result += radioPacket;
-  result += ",";
+  AddByteArray(radioPacket, sizeof(radioPacket));
+  
   dtostrf(AccEvent.acceleration.x, 3, 4, radioPacket);
-  result += radioPacket;
-  result += ",";
+  AddByteArray(radioPacket, sizeof(radioPacket));
+  
   dtostrf(AccEvent.acceleration.y, 3, 4, radioPacket);
-  result += radioPacket;
-  result += ",";
+  AddByteArray(radioPacket, sizeof(radioPacket));
+  
   dtostrf(AccEvent.acceleration.z, 3, 4, radioPacket);
-  result += radioPacket;
-  result += ",";
-*/
-  Serial.print("In the packet: "); Serial.println(result);
-  result.toCharArray(buff, 255);
+  AddByteArray(radioPacket, sizeof(radioPacket));
+  
+  Serial.print(F("In the packet: ")); Serial.println(ByteArray);
 
-  Serial.print("Sending "); Serial.println(buff);
-  Serial.println("Sending...");
-  rf95.send((uint8_t *)buff, 255);
-
-  /* Waiting for a reply */
+  Serial.println(F("Sending..."));
+  rf95.send((uint8_t *)ByteArray, sizeof(ByteArray));
+  
+  Serial.println(F("Waiting for packet to complete...")); 
+  rf95.waitPacketSent();
+  
+  // Now wait for a reply
   uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
   uint8_t len = sizeof(buf);
 
+  Serial.println(F("Waiting for reply..."));
   if (rf95.waitAvailableTimeout(100))
   { 
     // Should be a reply message for us now   
@@ -311,7 +334,8 @@ void ReadDOFAndPrint(){
    {
       Serial.print("Got reply: ");
       Serial.println((char*)buf);
-     
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);    
     }
     else
     {
@@ -322,6 +346,7 @@ void ReadDOFAndPrint(){
   {
     Serial.println("No reply, is there a listener around?");
   }
+
 }
 
 typedef struct sTask_tag{
@@ -330,6 +355,7 @@ typedef struct sTask_tag{
   void (*CallbackFcn)(void);
 } sTask;
 
+/* Task Scheduler - If new task, add task to scheduler */
 sTask TaskList[] = {
   //DAC Update
   {
@@ -369,3 +395,4 @@ void loop() {
   }
   delayMicroseconds(1000);
 }
+
